2d0eb86e6f8440279d94e9e3a89a61b16438dff5
Ruchigevariya <gevariyaruchita@gmail.com>
2022-06-23 18:40:30 +0530

    changes

---
+++ src/container/Medicines/Medicines.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Medicines(props) {
+    return (
+        <div>
+            <h2>Medicines</h2>
+        </div>
+    );
+}
+
+export default Medicines;
---
+++ src/container/Patients/Patients.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Patients(props) {
+    return (
+        <div>
+            <h2>patients</h2>
+        </div>
+    );
+}
+
+export default Patients;
