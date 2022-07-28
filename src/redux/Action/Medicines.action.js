b938363464a99fe69fbfbf7cb8cf7447432f954c
Ruchigevariya <gevariyaruchita@gmail.com>
2022-07-28 14:55:05 +0530

    chnage

--- src/redux/Action/Medicines.action.js
+++ src/redux/Action/Medicines.action.js
@@ -5,6 +5,7 @@ export const getMedicines = () => (dispatch) => {
         fetch('http://localhost:3004/medicines')
         .then((response) => response.json())
         .then((data) => dispatch({type: ActionTypes.GET_MEDICINESDATA, payload: data}))
+        .catch((error) => console.log(error))
     }catch (error) {
        console.log(error.message)
     }
